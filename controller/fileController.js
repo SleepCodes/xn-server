6c0148798f5a0fb950de793d181b60285bffb8af
ASUS Windows 10 <shencheng_104@aliyun.com>
2020-11-12 10:54:25 +0800

    new:文件上传之后保存进数据库

--- controller/fileController.js
+++ controller/fileController.js
@@ -6,32 +6,43 @@ const path = require('path')
 async function uploadFiles(ctx) {
 	// 单个文件
 	let file = ctx.request.files.file
-	// console.log(file)
+	let { _id } = ctx.request.body
 
 	// 创建可读流
 	const reader = fs.createReadStream(file.path)
 	// 文件的后缀名
-    let type = file.name.split('.')[1]
-    // 文件名
+	let type = file.name.split('.')[1]
+	// 文件名
 	let name = `${Date.now().toString(16)}.${type}`
 	// 文件保存路径
 	let filePath = path.join(__dirname, '../public/images/') + name
 	// 创建可写流
 	const upStream = fs.createWriteStream(filePath)
 	// 可读流通过管道写入可写流
-	const pro = new Promise((resolve, reject) => {
+	const url = new Promise((resolve, reject) => {
 		var stream = reader.pipe(upStream)
 
 		stream.on('finish', () => {
 			resolve(`http://192.168.1.73:3000/images/${name}`)
 		})
-    })
-    
-    //TODO 保存成功之后 需要将文件url 存入数据库
-	ctx.body = {
-		code: 200,
-		msg: '上传成功',
-		data: await pro
+	})
+	//保存成功之后 需要将文件url 存入数据库
+	let res = await fileServe.save(await url, _id)
+
+	if (res) {
+		ctx.body = {
+			code: 200,
+			msg: '上传并保存成功',
+			data: await url,
+			success: true
+		}
+	} else {
+		ctx.body = {
+			code: 200,
+			msg: '上传并保存成功',
+			data: await url,
+			success: false
+		}
 	}
 }
 
--- service/file.js
+++ service/file.js
@@ -1,8 +1,22 @@
 const FileModel = require('../model/hotel')
+const ObjectId = require('mongoose').Types.ObjectId
 
 // 文件上传之后 添加url 进数据库
-async function save(param) {
-    console.log(param)
+async function save(url, _id) {
+	let res = await FileModel.updateOne(
+		{ _id: ObjectId(_id) },
+		{
+			$push: {
+				images: url
+			}
+		}
+	)
+	if (res.nModified === 1) {
+		return true
+	} else {
+		console.log('修改失败')
+		return false
+	}
 }
 
 module.exports = {
